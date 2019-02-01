c8139092f399237ebad1d9210aa9b9d42c5ecb23
Fractured2K <fractured2k@gmail.com>
2019-02-01 10:59:29 -0800

    completed carousel constructor functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,16 @@
 class Carousel {
+    constructor(carousel) {
+        this.rightButton = carousel.querySelector('.right-button');
+        this.leftButton = carousel.querySelector('.left-button');
 
+        this.images = [...carousel.querySelectorAll('img')];
+
+        this.currentIndex = 0;
+        this.images[this.currentIndex].style.display = 'block';
+
+        this.leftButton.addEventListener('click', () => this.left());
+        this.rightButton.addEventListener('click', () => this.right());
+    }
 }
 
 let carousel = new Carousel(document.querySelector('.carousel'));
